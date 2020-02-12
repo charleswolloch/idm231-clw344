54ebc19e6d7970fcebd82380408e24a8da822fef
Charles Wolloch <clw344@drexel.edu>
2020-02-12 16:57:17 -0500

    functionality achieved!
    got everything working except audio

--- charactersigns.md
+++ charactersigns.md
@@ -1,38 +1,38 @@
 # Markdown for matching signs to characters
 https://www.bustle.com/p/what-zodiac-signs-game-of-thrones-characters-would-be-if-astrology-was-a-thing-in-westeros-17142897
 
-# 1. Aries: Daenerys Targaryen
+# 1. Aries: Daenerys Targaryen Mar 21.. Apr 19 
 * An Aries stands out because of their unique ability and drive to be the best leaders out there. They are bold, competitive, fearless, passionate people who take on whatever comes their way while managing to come out on top. Daenerys is pretty much a classic Aries. Her desire to be a leader is the main thing anyone notices about her, and she has shown a tremendous amount of strength and courage to get where she is today. Plus: fire sign.
 
-# 2. Taurus: Jaime Lannister
+# 2. Taurus: Jaime Lannister Apr 20.. May 20	
 * A Taurus is known for being very loyal, practical, responsible, stubborn, possessive, and romantic. This pretty much describes Jaime. He is incredibly loyal to his family and has proven throughout the series that he is true to his word no matter what, showing off both his stubborn and responsible sides. He is also a practical thinker, for the most part, except when it comes to decisions revolving around Cersei — then he is stubbornly romantic and loving in a very Taurus way.
 
-# 3. Gemini: Sansa Stark
+# 3. Gemini: Sansa Stark May 21.. Jun 21
 * Geminis represent two different personalities, which we have certainly seen from Sansa: she knows how to play the obedient, kind part just as well as she can play the ruthless and manipulative part. Geminis are very adaptable people who can learn quickly — Sansa has been through it all, and has managed to make every one of her situations work in the end. Geminis are also known for being social and quick-witted, two things that certainly describe Lady Stark.
 
-# 4. Cancer: Cersei Lannister
+# 4. Cancer: Cersei Lannister Jun 22.. Jul 22
 * Cancers can be challenging, but there is one thing that really makes them stand out, and that's their complete love and devotion to their family. Everything Cersei does, every decision she makes, centers around her total loyalty and devotion to her family members and her children, and that alone makes her a Cancer. But Cancers are also known to be moody, pessimistic, suspicious, and manipulative... so, yes, that is pretty much Cersei explained.
 
-# 5. Leo: Tyrion Lannister
+# 5. Leo: Tyrion Lannister Jul 23.. Aug 22
 * Leos are a sign that love to be the center of attention. They love to be where the action is, they're very sociable, they're loyal and dedicated, and they have some serious passion and drive. Tyrion is someone who always manages to put some sort of spotlight on himself, and he has never shied away from being in some sort of leadership position. Whatever he does, he puts his all into it, and he remains loyal to those he believes deserves his loyalty. And while he may have grown out of it a bit now, Tyrion was always someone who saw himself as a bit of celebrity... like a typical Leo.
 
-# 6. Virgo: Brienne of Tarth
+# 6. Virgo: Brienne of Tarth Aug 23.. Sep 22
 * There may be no other sign that is as loyal, hardworking, and practical as a Virgo is. Virgos are strong people who are detail-oriented and dedicated to being super organized and getting every job they are tasked with done right. This is Brienne in a nutshell. She is incredibly loyal to those she serves and her whole life revolves around getting her work done. She is completely focused on any task at hand and pays attention to every little detail. She is also a practical thinker, and at the same time, she can be self-critical, like a true Virgo.
 
-# 7. Libra: Bronn of the Blackwater
+# 7. Libra: Bronn of the Blackwater Sep 23.. Oct 23
 * Libras are diplomatic, social people who love to be around other people. They are also inspired by the finer things in life, and love to be surrounded by not only friends, but material possessions that are nice to look at. This is basically Bronn, who is obsessed with money and getting himself all of the riches he can get - it takes over everything else. Bronn is also known to always have a companion by his side, whether it's one of the Lannister brothers or a woman he's found.
 
-# 8. Scorpio: Arya Stark
+# 8. Scorpio: Arya Stark Oct 24.. Nov 21
 * A few words that describe Scorpios are mysterious, passionate, stubborn, brave, distrusting, secretive, and violent. What other sign could describe Arya in a better way? She is a woman of mystery and knows how to keep to herself to get exactly what she wants. She is brave to a fault, almost, and has a true violent side. She is never afraid of being assertive or acting like a leader, and at the same time, she can be highly emotional and moody. And, as we've seen, Arya clearly has the Scorpio sexual side as well.
 
-# 9. Sagittarius: Yigritte
+# 9. Sagittarius: Yigritte  Nov 22.. Dec 21
 * What a Sagittarius treasures above all else is that feeling of being totally free, which is all Yigritte has ever wanted and worked for. Sags are also known for their sense of humor, their impatience, and their enthusiasm. 
 
-# 10. Capricorn: Jon Snow
+# 10. Capricorn: Jon Snow Dec 22.. Jan 19
 * Capricorns are known for being responsible, disciplined people who make great managers but also tend to have a bit of a negative side. They are also very dedicated to their family lives and to doing things exactly right, and they tend to be very traditional and serious. These are all great ways to describe Jon. He is an excellent manager who has shown a decent amount of self-control, he is very responsible and is completely focused on his task at hand. He also has an extremely serious disposition and is known to be kind of a downer sometimes.
 
-# 11. Aquarius: Bran Stark
+# 11. Aquarius: Bran Stark Jan 20.. Feb 18
 * Anyone who is an Aquarius is probably known for being more unique than others. Aquarius is typically more eccentric and mythical; they are also very independent, aloof, and don't really show off their emotions. Still, an Aquarius knows how to be a fighter and a good friend, and they're exceptionally smart. They are deep thinkers who enjoy time to themselves and absolutely need to be using their mind all the time. Bran is definitely on the more eccentric side, and he is constantly sitting on his own, thinking and putting his mind to work. He doesn't appear to have any emotions, really, and he has shown that he doesn't need anyone by his side.
 
-# 12. Pisces: Hodor
+# 12. Pisces: Hodor Feb 19.. Mar 20
 * Pisces are probably the most sensitive sign of the zodiac. They are intuitive, gentle, compassionate people who have an artistic side, tend to be fearful, and hate cruelty. Hodor is a kind and generous person who does everything for his friends and other people. He's always willing to help the people he cares for, and anyone in need, and he is easily the most sensitive and compassionate person in the Game of Thrones universe.
--- master.js
+++ master.js
@@ -1,6 +1,24 @@
 let zodDateObj = document.getElementById('zodDate');
 let showMeObj = document.getElementById('showMe');
 
+let heroObj = document.getElementById('hero');
+
+
+let capObj = document.getElementById('cap');
+let ariObj = document.getElementById('ari');
+let scoObj = document.getElementById('sco');
+let sagObj = document.getElementById('sag');
+let aquObj = document.getElementById('aqu');
+let pisObj = document.getElementById('pis');
+let tauObj = document.getElementById('tau');
+let gemObj = document.getElementById('gem');
+let canObj = document.getElementById('can');
+let libObj = document.getElementById('lib');
+let leoObj = document.getElementById('leo');
+let virObj = document.getElementById('vir');
+
+
+
 function computeZod() {
     let userBday = zodDateObj.value;
     console.log('userBday is ' + userBday);
@@ -19,54 +37,170 @@ function dateToZodiac(whichMonth, whichDayOfMonth) {
     if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
         AstroSign = "Cap";
         gotSign = "Jon Snow";
-        gotZod = "Capricorns are known for being responsible, disciplined people who make great managers but also tend to have a bit of a negative side. They are also very dedicated to their family lives and to doing things exactly right, and they tend to be very traditional and serious. These are all great ways to describe Jon. He is an excellent manager who has shown a decent amount of self-control, he is very responsible and is completely focused on his task at hand. He also has an extremely serious disposition and is known to be kind of a downer sometimes.";
         
+        swapZodInfo(1);
+
     } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
         AstroSign = "Sag";
         gotSign = "Yigritte";
-        gotZod = "What a Sagittarius treasures above all else is that feeling of being totally free, which is all Yigritte has ever wanted and worked for. Sags are also known for their sense of humor, their impatience, and their enthusiasm.";
+        
+        swapZodInfo(4);
     } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
         AstroSign = "Sco";
         gotSign = "Arya Stark";
-        gotZod = "A few words that describe Scorpios are mysterious, passionate, stubborn, brave, distrusting, secretive, and violent. What other sign could describe Arya in a better way? She is a woman of mystery and knows how to keep to herself to get exactly what she wants. She is brave to a fault, almost, and has a true violent side. She is never afraid of being assertive or acting like a leader, and at the same time, she can be highly emotional and moody. And, as we've seen, Arya clearly has the Scorpio sexual side as well.";
+        
+        swapZodInfo(3);
+
     } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
         AstroSign = "Lib";
         gotSign = "Bronn of the Blackwater";
-        gotZod = "Libras are diplomatic, social people who love to be around other people. They are also inspired by the finer things in life, and love to be surrounded by not only friends, but material possessions that are nice to look at. This is basically Bronn, who is obsessed with money and getting himself all of the riches he can get - it takes over everything else. Bronn is also known to always have a companion by his side, whether it's one of the Lannister brothers or a woman he's found.";
+        
+        swapZodInfo(10);
     } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
         AstroSign = "Vir";
         gotSign = "Brienne of Tarth";
-        gotZod = "There may be no other sign that is as loyal, hardworking, and practical as a Virgo is. Virgos are strong people who are detail-oriented and dedicated to being super organized and getting every job they are tasked with done right. This is Brienne in a nutshell. She is incredibly loyal to those she serves and her whole life revolves around getting her work done. She is completely focused on any task at hand and pays attention to every little detail. She is also a practical thinker, and at the same time, she can be self-critical, like a true Virgo.";
+        
+        swapZodInfo(12);
     } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
         AstroSign = "Leo";
         gotSign = "Tyrion Lannister";
-        gotZod = "Leos are a sign that love to be the center of attention. They love to be where the action is, they're very sociable, they're loyal and dedicated, and they have some serious passion and drive. Tyrion is someone who always manages to put some sort of spotlight on himself, and he has never shied away from being in some sort of leadership position. Whatever he does, he puts his all into it, and he remains loyal to those he believes deserves his loyalty. And while he may have grown out of it a bit now, Tyrion was always someone who saw himself as a bit of celebrity... like a typical Leo.";
+        
+        swapZodInfo(11);
     } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
         AstroSign = "Can";
         gotSign = "Cersei Lannister";
-        gotZod = "Cancers can be challenging, but there is one thing that really makes them stand out, and that's their complete love and devotion to their family. Everything Cersei does, every decision she makes, centers around her total loyalty and devotion to her family members and her children, and that alone makes her a Cancer. But Cancers are also known to be moody, pessimistic, suspicious, and manipulative... so, yes, that is pretty much Cersei explained.";
+        
+        swapZodInfo(9);
     } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
         AstroSign = "Gem";
         gotSign = "Sansa Stark";
-        gotZod = "Geminis represent two different personalities, which we have certainly seen from Sansa: she knows how to play the obedient, kind part just as well as she can play the ruthless and manipulative part. Geminis are very adaptable people who can learn quickly — Sansa has been through it all, and has managed to make every one of her situations work in the end. Geminis are also known for being social and quick-witted, two things that certainly describe Lady Stark.";
+        
+        swapZodInfo(8);
     } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
         AstroSign = "Tau";
         gotSign = "Jaime Lannister";
-        gotZod = "A Taurus is known for being very loyal, practical, responsible, stubborn, possessive, and romantic. This pretty much describes Jaime. He is incredibly loyal to his family and has proven throughout the series that he is true to his word no matter what, showing off both his stubborn and responsible sides. He is also a practical thinker, for the most part, except when it comes to decisions revolving around Cersei — then he is stubbornly romantic and loving in a very Taurus way.";
+        
+        swapZodInfo(7);
     } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
         AstroSign = "Ari";
         gotSign = "Daenerys Targaryen";
-        gotZod = "An Aries stands out because of their unique ability and drive to be the best leaders out there. They are bold, competitive, fearless, passionate people who take on whatever comes their way while managing to come out on top. Daenerys is pretty much a classic Aries. Her desire to be a leader is the main thing anyone notices about her, and she has shown a tremendous amount of strength and courage to get where she is today. Plus: fire sign.";
+        
+        swapZodInfo(2);
     } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
         AstroSign = "Pis";
         gotSign = "Hodor";
-        gotZod = "Pisces are probably the most sensitive sign of the zodiac. They are intuitive, gentle, compassionate people who have an artistic side, tend to be fearful, and hate cruelty. Hodor is a kind and generous person who does everything for his friends and other people. He's always willing to help the people he cares for, and anyone in need, and he is easily the most sensitive and compassionate person in the Game of Thrones universe.";
+        
+        swapZodInfo(6);
     } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
         AstroSign = "Aqu";
         gotSign = "Bran Stark";
-        gotZod = "Anyone who is an Aquarius is probably known for being more unique than others. Aquarius is typically more eccentric and mythical; they are also very independent, aloof, and don't really show off their emotions. Still, an Aquarius knows how to be a fighter and a good friend, and they're exceptionally smart. They are deep thinkers who enjoy time to themselves and absolutely need to be using their mind all the time. Bran is definitely on the more eccentric side, and he is constantly sitting on his own, thinking and putting his mind to work. He doesn't appear to have any emotions, really, and he has shown that he doesn't need anyone by his side.";
+        
+        swapZodInfo(5);
     }
 
-    return gotSign + ' - ' + gotZod;
+   // return gotSign + ' - ' + gotZod;
+
+}
+
+
+capObj.addEventListener('click', function () {
+
+    swapZodInfo(1);
+}, false);
+
+ariObj.addEventListener('click', function () {
+
+    swapZodInfo(2);
+}, false);
+
+scoObj.addEventListener('click', function () {
+
+    swapZodInfo(3);
+}, false);
+
+sagObj.addEventListener('click', function () {
+
+    swapZodInfo(4);
+}, false);
+
+aquObj.addEventListener('click', function () {
+
+    swapZodInfo(5);
+}, false);
+
+pisObj.addEventListener('click', function () {
+
+    swapZodInfo(6);
+}, false);
+
+tauObj.addEventListener('click', function () {
+
+    swapZodInfo(7);
+}, false);
+
+gemObj.addEventListener('click', function () {
+
+    swapZodInfo(8);
+}, false);
+
+canObj.addEventListener('click', function () {
+
+    swapZodInfo(9);
+}, false);
+
+libObj.addEventListener('click', function () {
+
+    swapZodInfo(10);
+}, false);
+
+leoObj.addEventListener('click', function () {
+
+    swapZodInfo(11);
+}, false);
+
+virObj.addEventListener('click', function () {
+
+    swapZodInfo(12);
+}, false);
+
+
+
+function swapZodInfo(whichOne) {
+    // swap out zodiac image
+    // swap out zodiac description with date range
+    // play a zodiac sound
+
+    console.log('swap zod info called ' + whichOne);
+
+    if (whichOne == 1) {
+        heroObj.src = 'images/cap_hero.png';
+    } else if (whichOne == 2) {
+        heroObj.src = 'images/ari_hero.png';
+    } else if (whichOne == 3) {
+        heroObj.src = 'images/sco_hero.png';
+    } else if (whichOne == 4) {
+        heroObj.src = 'images/sag_hero.png';
+    } else if (whichOne == 5) {
+        heroObj.src = 'images/aqu_hero.png';
+    } else if (whichOne == 6) {
+        heroObj.src = 'images/pis_hero.png';
+    } else if (whichOne == 7) {
+        heroObj.src = 'images/tau_hero.png';
+    } else if (whichOne == 8) {
+        heroObj.src = 'images/gem_hero.png';
+    } else if (whichOne == 9) {
+        heroObj.src = 'images/can_hero.png';
+    } else if (whichOne == 10) {
+        heroObj.src = 'images/lib_hero.png';
+    } else if (whichOne == 11) {
+        heroObj.src = 'images/leo_hero.png';
+    } else if (whichOne == 12) {
+        heroObj.src = 'images/vir_hero.png';
+    } else {
+        console.log('error in swapzodinfo');
+    }
+
+
+
 
 }
