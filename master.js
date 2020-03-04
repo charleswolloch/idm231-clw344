e66dc37c78ea4fa92ec6f762e8788fc8e1f1af27
Charles Wolloch <clw344@drexel.edu>
2020-03-04 11:14:55 -0500

    edited code to get rid of error message while running

--- master.js
+++ master.js
@@ -38,7 +38,7 @@ function computeZod() {
     let monNum = parseInt(monStr);
     let dayNum = parseInt(dayStr);
 
-    showMeObj.innerHTML = dateToZodiac(monNum, dayNum);
+    dateToZodiac(monNum, dayNum);
 }
 
 
@@ -46,68 +46,68 @@ function dateToZodiac(whichMonth, whichDayOfMonth) {
     if ((whichMonth == 12 && whichDayOfMonth >= 22) || (whichMonth == 1 && whichDayOfMonth <= 19)) {
         AstroSign = "Cap";
         gotSign = "Jon Snow";
-        
+
         swapZodInfo(1);
 
     } else if ((whichMonth == 11 && whichDayOfMonth >= 22) || (whichMonth == 12 && whichDayOfMonth <= 21)) {
         AstroSign = "Sag";
         gotSign = "Yigritte";
-        
+
         swapZodInfo(4);
     } else if ((whichMonth == 10 && whichDayOfMonth >= 24) || (whichMonth == 11 && whichDayOfMonth <= 21)) {
         AstroSign = "Sco";
         gotSign = "Arya Stark";
-        
+
         swapZodInfo(3);
 
     } else if ((whichMonth == 9 && whichDayOfMonth >= 23) || (whichMonth == 10 && whichDayOfMonth <= 23)) {
         AstroSign = "Lib";
         gotSign = "Bronn of the Blackwater";
-        
+
         swapZodInfo(10);
     } else if ((whichMonth == 8 && whichDayOfMonth >= 23) || (whichMonth == 9 && whichDayOfMonth <= 22)) {
         AstroSign = "Vir";
         gotSign = "Brienne of Tarth";
-        
+
         swapZodInfo(12);
     } else if ((whichMonth == 7 && whichDayOfMonth >= 23) || (whichMonth == 8 && whichDayOfMonth <= 22)) {
         AstroSign = "Leo";
         gotSign = "Tyrion Lannister";
-        
+
         swapZodInfo(11);
     } else if ((whichMonth == 6 && whichDayOfMonth >= 22) || (whichMonth == 7 && whichDayOfMonth <= 22)) {
         AstroSign = "Can";
         gotSign = "Cersei Lannister";
-        
+
         swapZodInfo(9);
     } else if ((whichMonth == 5 && whichDayOfMonth >= 21) || (whichMonth == 6 && whichDayOfMonth <= 21)) {
         AstroSign = "Gem";
         gotSign = "Sansa Stark";
-        
+
         swapZodInfo(8);
     } else if ((whichMonth == 4 && whichDayOfMonth >= 20) || (whichMonth == 5 && whichDayOfMonth <= 20)) {
         AstroSign = "Tau";
         gotSign = "Jaime Lannister";
-        
+
         swapZodInfo(7);
     } else if ((whichMonth == 3 && whichDayOfMonth >= 21) || (whichMonth == 4 && whichDayOfMonth <= 19)) {
         AstroSign = "Ari";
         gotSign = "Daenerys Targaryen";
-        
+
         swapZodInfo(2);
     } else if ((whichMonth == 2 && whichDayOfMonth >= 19) || (whichMonth == 3 && whichDayOfMonth <= 20)) {
         AstroSign = "Pis";
         gotSign = "Hodor";
-        
+
         swapZodInfo(6);
     } else if ((whichMonth == 1 && whichDayOfMonth >= 20) || (whichMonth == 2 && whichDayOfMonth <= 18)) {
         AstroSign = "Aqu";
         gotSign = "Bran Stark";
-        
+
         swapZodInfo(5);
     }
 
-   // return gotSign + ' - ' + gotZod;
+    // return gotSign + ' - ' + gotZod;
 
 }
 
@@ -140,7 +140,7 @@ aquObj.addEventListener('click', function () {
 pisObj.addEventListener('click', function () {
 
     swapZodInfo(6);
-    
+
 }, false);
 
 tauObj.addEventListener('click', function () {
